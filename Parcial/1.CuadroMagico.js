91dece45e4ff9345a23e6f17b76092084fc44edc
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2022-03-18 14:56:55 -0500

    fghjk

--- Parcial/1.CuadroMagico.js
+++ Parcial/1.CuadroMagico.js
@@ -1,41 +1,80 @@
+// var array = [[],[],[]]; 
+
+// var nfilas = 3;
+// var nColums = 3;
+// for (var i = 0; i < nfilas; i++) {//filas 
+//   for (var j = 0; j < nColums; j++) {//columnas
+//     var num = prompt("["+i+","+j+"]"+'. Ingrese un numero:',"");
+//     array[i].push(parseInt(num))
+//     document.write(array[i][j])
+//     document.write("["+i+","+j+"]")
+//   }
+//   document.write("<br>");
+// }
+// // document.write(array)
+// alert(array);
+
+
+
+
 const array = [
   [2, 7, 6],
   [9, 5, 1],
   [4, 3, 8],
 ];
 
-const N = 3;
+for (var i = 0; i < 3; i++) {//filas 
+  for (var j = 0; j < 3; j++) {//columnas
+    document.write(array[i][j])
+    document.write("["+i+","+j+"]")
+  }
+  document.write("<br>");
+}
 
-magico(array, N);
 
-function magico(array, N) {
-  var numMagico = 0;
-  for (var j = 0; j < N; j++) {
+
+const n = 3;
+
+magico(array, n);
+
+function magico(array, n) {
+  var numMagico = 0, sumaDiag1 = 0, sumaDiag2 = 0;
+
+  for (var j = 0; j < n; j++) {
     numMagico += array[0][j];
   }
+  
+  for (var i = 0; i < n; i++) {
 
-  var sumaDiag1 = 0,
-    sumaDiag2 = 0;
-  for (var i = 0; i < N; i++) {
-    var sumaFila = 0,
-      sumaCol = 0;
-    for (var j = 0; j < N; j++) {
+    var sumaFila = 0, sumaCol = 0;
+
+    for (var j = 0; j < n; j++) {
       sumaFila += array[i][j];
       sumaCol += array[j][i];
     }
+
     if (sumaFila != numMagico || sumaCol != numMagico) {
-      console.log("NO es un cuadrado magico");
+      console.log("nO es un cuadrado magico");
+      document.write("nO es un cuadrado magico");
       return false;
     }
+
     sumaDiag1 += array[i][i];
-    sumaDiag2 += array[N - 1 - i][N - 1 - i];
+    sumaDiag2 += array[n - 1 - i][n - 1 - i];
+    
+    // console.log(i+","+i)
+    // console.log(array[i][i])
+    console.log(array[n - 1 - i][n - 1 - i])
   }
+  
   if (sumaDiag1 != numMagico || sumaDiag2 != numMagico) {
-    console.log("NO es un cuadrado magico");
+    console.log("nO es un cuadrado magico");
+    document("nO es un cuadrado magico");
     return false;
   }
 
   console.log("Es un cuadrado magico");
+  document.write("Es un cuadrado magico");
   return true;
   console.log(array);
 }
