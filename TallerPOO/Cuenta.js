ef1cae9e54ccd339cc518dbb43a26b6d827061d4
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2022-03-30 23:23:28 -0500

    taller segundo corte lenguaje de programacion

---
+++ TallerPOO/Cuenta.js
@@ -0,0 +1,53 @@
+
+class Cuenta
+{
+    // var strTitular;
+    #strTitular;
+    #intCantidad;
+
+    constructor (titular, cantidad)
+    {
+        this.titular = titular;
+        this.cantidad = cantidad;
+    }
+    // function gettitular(x){
+    get getTitularCuenta(){
+        return this.titular;
+    }
+    set setTitularCuenta(titular){
+        return this.titular = titular;
+    }
+    get getCantidadCueta(){
+        return this.cantidad = cantidad;
+    }
+    set setCantidadCueta(cuenta){
+        return this.cantidad = cantidad;
+    }
+
+    ingresar(adicion) {
+       
+        if(this.cantidad<0){
+            console.log("No puede ingresar numeros negativos")
+        }else{
+            this.cantidad += adicion;
+        }
+        return this.cantidad;
+    }
+
+    retirar(sustraccion){
+    
+        if(sustraccion<0){
+            this.cantidad += sustraccion;
+            return this.cantidad;
+        }else{
+            console.log("No puede ingresar numeros positivos")   
+        }
+    }
+}
+var cantidad = 7;
+let cuenta = new Cuenta("Armando",cantidad);
+var adiccion = 11;
+var total = cuenta.ingresar(adiccion);
+console.log("Cantidad actual "+cantidad+" Ingresa: "+adiccion+" Total: "+total);
+var sustraccion = -3;
+console.log("se restan: "+sustraccion+" quedan :"+cuenta.retirar(sustraccion));
