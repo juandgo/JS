5c6201f48bd0e8dc9d2e3c97d0f3edcc832ddf8d
JuanDa97 <juandavidgo1997@gmail.com>
2022-04-01 13:40:35 -0500

    primer ejercicio

--- TallerPOO/Cuenta.js
+++ TallerPOO/Cuenta.js
@@ -1,53 +1,71 @@
 
-class Cuenta
-{
-    // var strTitular;
-    #strTitular;
-    #intCantidad;
+class CuentaAhorro  {
 
-    constructor (titular, cantidad)
+    #strTitular = "";
+    #cantidad = 0;
+
+    constructor(titular, cantidad)
+    {
+      this.#strTitular = titular;
+      this.#cantidad = cantidad;
+    } 
+
+    get getTitular() 
+    {
+       return this.#strTitular;
+     }
+
+    set setTitular(titular) 
     {
         this.titular = titular;
-        this.cantidad = cantidad;
-    }
-    // function gettitular(x){
-    get getTitularCuenta(){
-        return this.titular;
-    }
-    set setTitularCuenta(titular){
-        return this.titular = titular;
-    }
-    get getCantidadCueta(){
-        return this.cantidad = cantidad;
-    }
-    set setCantidadCueta(cuenta){
-        return this.cantidad = cantidad;
     }
 
-    ingresar(adicion) {
-       
-        if(this.cantidad<0){
+    get getCantidad() 
+    {
+       var cantidad = parseInt(this.#cantidad);
+       return cantidad ;
+     }
+
+    set setCantidad(cantidad) 
+    {
+        this.#cantidad = cantidad;
+    }
+
+    ingresar(adicion) 
+    {
+        if(this.#cantidad<0){
             console.log("No puede ingresar numeros negativos")
         }else{
-            this.cantidad += adicion;
+            this.#cantidad += adicion;
         }
-        return this.cantidad;
+        return this.#cantidad;
     }
 
-    retirar(sustraccion){
-    
-        if(sustraccion<0){
-            this.cantidad += sustraccion;
-            return this.cantidad;
+    retirar(valor)
+    {
+        // var cantidad = prompt("Digite el valor de la cantidad a retirar");
+        if(valor>this.#cantidad){
+            // console.log("\n¡Fondos Insuficientes!,No se puede retirar mas de los que hay en la cuenta.");  
+            var mensaje = "\n¡Fondos Insuficientes!,No se puede retirar mas de los que hay en la cuenta.";  
+            return mensaje;   
         }else{
-            console.log("No puede ingresar numeros positivos")   
+            var retorno = this.#cantidad - valor;
+            return retorno;
         }
     }
-}
-var cantidad = 7;
-let cuenta = new Cuenta("Armando",cantidad);
-var adiccion = 11;
-var total = cuenta.ingresar(adiccion);
-console.log("Cantidad actual "+cantidad+" Ingresa: "+adiccion+" Total: "+total);
-var sustraccion = -3;
-console.log("se restan: "+sustraccion+" quedan :"+cuenta.retirar(sustraccion));
+
+    toString()
+    {
+        var mensaje = "La cantidad dinero anteriormente era de: $"+cantidad+"\nSu ingreso fue de: $"+adiccion+"\nTotal: $"+total+"\nSu retiro fue de: $"+sustraccion+"\nSu cantidad de dienero actual es de: $"+cuenta.retirar(sustraccion);
+        return mensaje;
+    }
+  }
+  var cantidad = 7;
+  var cuenta = new CuentaAhorro("Armando",cantidad);
+  var adiccion = 11;
+  var total = cuenta.ingresar(adiccion);
+  var sustraccion = 16;
+  console.log(cuenta.toString());
+
+
+
